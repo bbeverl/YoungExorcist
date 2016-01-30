64db46b15ff3115c3931df3cc07ccd0416ba90c2
Ben Beverly <bbeverl@gmail.com>
2016-01-30 11:49:58 -0500

    First code and scene commit. We can now draw lines and have some pattern detection

---
+++ Assets/Scripts/RitualDrawing/RitualNode.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(Collider)) ]
+public class RitualNode : MonoBehaviour {
+
+	public bool IsHit {
+		get; private set;
+	}
+
+	public bool DetectHit (Vector3 originPoint, Vector3 endPoint)
+	{
+		Collider collider = gameObject.GetComponent<Collider>();
+		if(collider.bounds.Contains(originPoint) || collider.bounds.Contains(endPoint)) {
+			return true;
+		}
+
+		RaycastHit hitInfo;
+		if(Physics.Raycast(endPoint, endPoint - originPoint,out hitInfo, (endPoint - originPoint).magnitude)) {
+			if(hitInfo.collider.gameObject == this.gameObject) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public void Hit ()
+	{
+		IsHit = true;
+
+		Renderer renderer = this.GetComponent<Renderer>();
+		if(renderer != null) {
+			renderer.material.color = Color.blue;
+		}
+	}
+
+	public void ResetHit ()
+	{
+		IsHit = false;
+
+		Renderer renderer = this.GetComponent<Renderer>();
+		if(renderer != null) {
+			renderer.material.color = Color.white;
+		}
+	}
+
+}
---
+++ Assets/Scripts/RitualDrawing/RitualNodeController.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class RitualNodeController : MonoBehaviour {
+
+	[SerializeField]
+	private DrawLine lineDrawer = null;
+
+	[SerializeField]
+	private List<RitualNode> ritualNodes = new List<RitualNode>();
+
+	[SerializeField]
+	private List<int> hitSequence = new List<int> ();
+
+	private void OnEnable () {
+		if(ritualNodes.Count < 1) {
+			RitualNode[] nodes = GetComponentsInChildren<RitualNode>();
+			ritualNodes.AddRange(nodes);
+		}
+
+		if(lineDrawer == null) {			
+			lineDrawer = GetComponentInChildren<DrawLine>();
+		}
+		if(lineDrawer != null) {
+			lineDrawer.OnLinePointAdded += OnLinePointAdded;
+			lineDrawer.OnLineDrawingStopped += OnDrawingStopped;
+		}
+	}
+
+	private void OnDisable () 
+	{
+		if(lineDrawer != null) {
+			lineDrawer.OnLinePointAdded -= OnLinePointAdded;
+			lineDrawer.OnLineDrawingStopped -= OnDrawingStopped;
+		}
+	}
+	
+	private void OnLinePointAdded (Vector3 previousPoint, Vector3 newPoint)
+	{
+		for (int i = 0, ritualNodesCount = ritualNodes.Count; i < ritualNodesCount; i++) {
+			RitualNode node = ritualNodes [i];
+
+			if (node.DetectHit (previousPoint, newPoint)) {
+				node.Hit ();
+				if(hitSequence.Count == 0 || hitSequence[hitSequence.Count - 1] != i) {
+					hitSequence.Add(i);
+				}
+			}
+		}
+	}
+
+	private void OnDrawingStopped ()
+	{
+		StartCoroutine(FinishRitual());
+	}
+
+	private System.Collections.IEnumerator FinishRitual ()
+	{
+		yield return new WaitForSeconds(1);
+
+		lineDrawer.ResetLines ();
+
+		for (int i = 0, ritualNodesCount = ritualNodes.Count; i < ritualNodesCount; i++) {
+			ritualNodes[i].ResetHit();
+		}
+
+		hitSequence.Clear();
+	}
+}
