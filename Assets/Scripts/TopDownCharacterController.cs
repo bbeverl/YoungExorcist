b0549af61df009e9cde853bb804f5f838cc8b03e
Ben Beverly <bbeverl@gmail.com>
2016-01-31 00:31:00 -0500

    We can now move Igooooor. And I forgot the tag manager before

---
+++ Assets/Scripts/TopDownCharacterController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TopDownCharacterController : MonoBehaviour {
+
+    private float moveSpeed = 3;
+
+    public Rigidbody myRigidbody;
+
+    void Start ()
+    {
+        if(myRigidbody == null) {
+            myRigidbody = GetComponent<Rigidbody> ();
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if(GameManager.Instance.RitualController.IsRitualRunning == false) {
+            Vector3 inputMovement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+            //myRigidbody.MovePosition((myRigidbody.position + AxisInput) * Time.fixedDeltaTime * Speed);
+            transform.Translate(inputMovement*Time.deltaTime*moveSpeed, Space.World);
+        }
+    }
+}
