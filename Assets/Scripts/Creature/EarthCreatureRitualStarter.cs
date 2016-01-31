a23efe8855298bef57ef5d6bb5fcf506b7f2abc3
Ben Beverly <bbeverl@gmail.com>
2016-01-31 15:36:08 -0500

    Adding the monsters

--- Assets/Scripts/Creature/EarthCreatureRitualStarter.cs
+++ Assets/Scripts/Creature/EarthCreatureRitualStarter.cs
@@ -2,10 +2,10 @@
 using System.Collections;
 using System.Linq;
 
-public class FireCreatureRitualStarter : CreatureRitualStarter {
+public class EarthCreatureRitualStarter : CreatureRitualStarter {
 
 	// This is the water sequance, fire is weak to water.
-    private int[] mySequence = RitualSequences.FireSequence;
+    private int[] mySequence = RitualSequences.EarthSequence;
 
 	#region implemented abstract members of CreatureRitualStarter
 	protected override bool CheckRitualSequence (int[] sequence)
--- Assets/Scripts/Creature/WaterCreatureRitualStarter.cs
+++ Assets/Scripts/Creature/WaterCreatureRitualStarter.cs
@@ -2,10 +2,10 @@
 using System.Collections;
 using System.Linq;
 
-public class FireCreatureRitualStarter : CreatureRitualStarter {
+public class WaterCreatureRitualStarter : CreatureRitualStarter {
 
 	// This is the water sequance, fire is weak to water.
-    private int[] mySequence = RitualSequences.FireSequence;
+	private int[] mySequence = RitualSequences.WaterSequence;
 
 	#region implemented abstract members of CreatureRitualStarter
 	protected override bool CheckRitualSequence (int[] sequence)
