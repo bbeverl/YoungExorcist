871f846d2b352bf89d1907577792efeedf383b69
TrobFrank <trobfrank92@gmail.com>
2016-01-31 14:19:22 -0500

    added SFX + player sound script

---
+++ Assets/Scripts/PlayerSound.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerSound : MonoBehaviour {
+
+	public AudioSource[] audioClip;
+
+
+	// Use this for initialization
+	void Start () {
+	
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+
+		//PlaySound (index of array on player);
+
+
+	}
+
+	
+	void PlaySound (int clip){
+		
+		audio.source = audioClip[clip];
+		audio.Play ();
+		
+	}
+
+
+}
